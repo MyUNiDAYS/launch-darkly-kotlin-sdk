61b903c6a0503dffa3b3d6b9b127891e7f88b3e4
Andrew Reed <andrew_reed@hotmail.com>
2024-06-04 14:44:25 +0100

    Adjusted compiler arguments to be inside pod file

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -65,8 +65,7 @@ kotlin {
             isStatic = true
         }
         pod("LaunchDarkly") {
-            version = "9.8.0"
-            extraOpts = listOf("-compiler-option", "-LD_OBJC_EXCLUDE_PURE_SWIFT_APIS=1")
+            version = "9.8.1"
         }
     }
 
@@ -98,6 +97,12 @@ kotlin {
         val iosTest by getting
         val iosSimulatorArm64Test by getting
         iosSimulatorArm64Test.dependsOn(iosTest)
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.targets.native.tasks.PodGenTask>().configureEach {
+    doLast {
+        podfile.get().apply { writeText(readText().replace("config.build_settings['CODE_SIGNING_ALLOWED'] = \"NO\"", "config.build_settings['CODE_SIGNING_ALLOWED'] = \"NO\"\nconfig.build_settings['SWIFT_ACTIVE_COMPILATION_CONDITIONS'] = '\$(inherited) LD_OBJC_EXCLUDE_PURE_SWIFT_APIS'\n")) }
     }
 }
 
