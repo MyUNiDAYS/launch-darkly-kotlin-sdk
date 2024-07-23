f456769ec52ebaf98cdf76cff3e35c679dd15b93
Andrew Reed <andrew_reed@hotmail.com>
2024-07-23 09:58:50 +0100

    Removed usages of reflection in android implementation

--- library/src/androidMain/kotlin/com/myunidays/launchdarkly/EvaluationDetail.android.kt
+++ library/src/androidMain/kotlin/com/myunidays/launchdarkly/EvaluationDetail.android.kt
@@ -10,20 +10,15 @@ actual class EvaluationDetail<T> internal constructor(
             value: T?,
             variationIndex: Int,
             reason: com.launchdarkly.sdk.EvaluationReason?
-        ): EvaluationDetail<T> {
-            val sdkDetail = createEvaluationDetail(value, variationIndex, reason)
-            return EvaluationDetail(sdkDetail)
-        }
+        ): EvaluationDetail<T> =
+            EvaluationDetail(createEvaluationDetail(value, variationIndex, reason))
 
         private fun <T> createEvaluationDetail(
             value: T?,
             variationIndex: Int,
             reason: com.launchdarkly.sdk.EvaluationReason?
-        ): com.launchdarkly.sdk.EvaluationDetail<T> {
-            // Use reflection to access the private constructor
-            val constructor = com.launchdarkly.sdk.EvaluationDetail::class.constructors.first()
-            return constructor.call(value, variationIndex, reason) as com.launchdarkly.sdk.EvaluationDetail<T>
-        }
+        ): com.launchdarkly.sdk.EvaluationDetail<T> =
+            com.launchdarkly.sdk.EvaluationDetail.fromValue(value, variationIndex, reason)
     }
 
     override val value: T?
